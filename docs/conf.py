d5af4126bb230093a30ae0771f62376d76538160
Patrick Forringer <patrick@forringer.com>
2021-01-10 14:51:46 -0600

    Bump version: 0.1.12 → 0.1.13a1

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ author = "Patrick Forringer, Buddy Lindsey"
 # the built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = "0.1.12"
+release = "0.1.13"
 # The short X.Y.Z version.
 version = re.match(r"^([0-9]+\.){2}[0-9]+", release).group(0)
 
